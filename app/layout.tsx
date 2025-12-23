ce46115af5d5d89826bc812e53a6bc2d83685186
Kyle Worrall <65330257+kylewhirl@users.noreply.github.com>
2025-12-22 19:18:32 -0800

    Use relative image paths

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
     locale: "en_US",
     images: [
       {
-        url: "/images/feature.png.webp",
+        url: "images/feature.png.webp",
         width: 1200,
         height: 630,
         alt: "Exterior of Taco Shop in Reno",
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title,
     description,
-    images: ["/images/feature.png.webp"],
+    images: ["images/feature.png.webp"],
   },
   icons: {
     icon: [{ url: "/favicon.ico", type: "image/x-icon" }],
